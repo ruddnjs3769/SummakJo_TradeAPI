8f73a3d8c3b8b9761af15838da9c74539a3a53a7
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-06-01 21:43:47 +0900

    🎨 docs : payment pages구조 추가

---
+++ src/pages/payment/checkOrder/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+export default function CheckOrder() {
+  return (
+    <div className={styles.container}>
+      <h1>주문을 검토하세요!!!!</h1>
+    </div>
+  )
+}
