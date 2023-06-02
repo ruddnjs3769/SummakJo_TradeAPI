c5987cbf22a4ee107a6a5a2901e2a6a20654c4ec
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-06-02 22:02:06 +0900

    🔧 feat : recoil추가 recoil 추가 레이아웃 작업을 위한 dummyData추가

---
+++ src/components/payment/PayProcessFlow.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function PayProcessFlow() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <h3>장바구니</h3>
+        </li>
+        <li>
+          <h3>약관 동의</h3>
+        </li>
+        <li>
+          <h3>구매</h3>
+        </li>
+        <li>
+          <h3>결제</h3>
+        </li>
+        <li>
+          <h3>주문 검토</h3>
+        </li>
+      </ul>
+    </div>
+  )
+}
