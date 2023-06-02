c860ae6723a22a1cb41ed2cb589a6141a95a2a45
leechanghwi <lih120@naver.com>
2023-06-02 13:06:18 +0900

    access pages 구조추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,26 @@ import { Route, Routes } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import UserLayout from './layouts/UserLayout'
 import Search from './pages/search'
+import Access from './pages/access'
+import AgeSignUp from './pages/access/ageSignUp'
+import SignUpForm from './pages/access/signUpForm'
+import LogIn from './pages/access/logIn'
+import LogOut from './pages/access/logOut'
+import PasswordCheck from './pages/access/passwordCheck'
+import PasswordChangeForm from './pages/access/passwordChangeForm'
+
 function App() {
   return (
     <Routes>
       <Route path="/user" element={<UserLayout />}>
         <Route path="/user/mypage/:username" element={<Mypage />} />
+        <Route path="/user/access/" element={<Access />} />
+        <Route path="/user/access/agesignup" element={<AgeSignUp />} />
+        <Route path="/user/access/signupform" element={<SignUpForm />} />
+        <Route path="/user/access/login" element={<LogIn />} />
+        <Route path="/user/access/logout" element={<LogOut />} />
+        <Route path="/user/access/passwordcheck" element={<PasswordCheck />} />
+        <Route path="/user/access/passwordchangeform" element={<PasswordChangeForm />} />
       </Route>
       <Route path="/" element={<MainLayout />}>
         <Route path="/" element={<Home />} />
---
+++ src/pages/access/passwordCheck/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+export default function PasswordCheck() {
+  return (
+    <div className={styles.container}>
+      <h1>1</h1>
+    </div>
+  )
+}
