2bb199531ff8685c77d4c494e21e47229a08f9ac
Seongeun Kim <kse3447@gmail.com>
2023-06-02 16:34:28 +0900

    :fire:bank종류별파일 삭제및파일명통일

--- src/pages/mypage/jjimItems/index.tsx
+++ src/pages/mypage/jjimItems/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './index.module.scss'
 
-export default function Jjimitems() {
+export default function JjimItems() {
   return (
     <div className={styles.container}>
         <h1>마이페이지-찜한상품</h1>
